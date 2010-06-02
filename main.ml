e07e7a5623847ac68054cd352e54c302265b4501
a_m0d <damien.schoof@gmail.com>
2010-06-02 23:20:06 +0800

    Removed some unneeded code and added some comments

--- main.ml
+++ main.ml
@@ -1,3 +1,4 @@
+(* Print a message to the terminal *)
 let print msg () =
     print_endline msg;
     flush stdout
@@ -9,24 +10,31 @@ let delete_event ev =
     flush stdout;
     false
 
+(* Close our main window *)
 let destroy () = GMain.Main.quit ()
 
 (* Entries for the file menu *)
 let file_menu_entries = [
     `I ("New", print "New");
+    `I ("Open", print "Open");
+    `I ("Save", print "Save");
+    `I ("Save As", print "Save as");
     `S;
     `I ("Quit", GMain.Main.quit)
 ]
 
-let menu_entries = [
-    `M ("File", file_menu_entries);
+(* Entries for the help menu *)
+let help_menu_entries = [
+    `I ("About", print "About")
 ]
 
+(* Helper function for creating the menubar *)
 let create_menu label menubar =
     let item = GMenu.menu_item ~label ~packing:menubar#append () in
     GMenu.menu ~packing:item#set_submenu ()
 
 let main () =
+    (* Setup the window *)
     GMain.Main.init ();
     let window = GWindow.window ~title:"Dromadie" () in
     window#event#connect#delete ~callback:delete_event;
@@ -37,6 +45,8 @@ let main () =
     let menubar = GMenu.menu_bar ~packing:(vbox1#pack ~expand:false) () in
     let menu = create_menu "File" menubar in
     GToolbox.build_menu menu ~entries:file_menu_entries;
+    let menu = create_menu "Help" menubar in
+    GToolbox.build_menu menu ~entries:help_menu_entries;
 
     (* Create the toolbar *)
     let toolbar = GButton.toolbar ~packing:(vbox1#pack ~expand:false) () in
@@ -46,12 +56,18 @@ let main () =
 
     (* The body of the window *)
     let hbox1 = GPack.hbox ~spacing:5 ~packing:(vbox1#pack ~expand:true) () in
+
+    (* The browser on the left *)
     GMisc.label ~text:"Tree placeholder" ~packing:hbox1#pack ();
+
+    (* And the sourceviews on the right *)
     let source_notebook = GPack.notebook ~packing:(hbox1#pack ~expand:true) () in
     let source_view = GSourceView.source_view ~width:640 ~height:480
         ~auto_indent:true ~insert_spaces_instead_of_tabs:true
         ~show_line_numbers:true ~tabs_width:4 ~margin:80 ~show_margin:true () in
     source_notebook#append_page source_view#coerce;
+
+    (* Show the window *)
     window#set_allow_shrink true;
     window#show ();
     GMain.Main.main ()
