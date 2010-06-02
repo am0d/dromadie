7859e47c56f239e79e8795cce4059e733a6a1893
a_m0d <damien.schoof@gmail.com>
2010-06-02 20:31:04 +0800

    Removed need for the gtkInit link; connected delete and destroy event handlers up

--- main.ml
+++ main.ml
@@ -1,6 +1,18 @@
+(* We need this here because eventually we will need to ask the user if the are
+sure they want to quit *)
+let delete_event ev =
+    print_endline "Delete event occurred";
+    flush stdout;
+    false
+
+let destroy () = GMain.Main.quit ()
+
 let main () =
+    ignore (GMain.Main.init ());
     let window = GWindow.window () in
+    ignore (window#event#connect#delete ~callback:delete_event);
+    ignore (window#connect#destroy ~callback:destroy);
     window#show ();
-    GMain.main ()
+    GMain.Main.main ()
 
 let _ = main ()
