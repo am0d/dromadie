4161a46d5be7bcd0834d49c0535f93b38d10363a
a_m0d <damien.schoof@gmail.com>
2010-07-09 22:00:09 +0800

    Added some comments to describe the functions in util.ml Also fixed a bug in Util.extension that would throw a Not_found exception if a filename had no extension

--- src/util.ml
+++ src/util.ml
@@ -1,17 +1,26 @@
 (* Parses the command line arguments and returns an array containing all the
-files specified on the command line *)
+ * files specified on the command line
+ *)
 let parse_args () =
     if (Array.length Sys.argv) > 1 then
         Array.sub Sys.argv 1 (Array.length Sys.argv - 1)
     else
         Array.make 1 ""
 
+(* Checks if a string (str) ends with another string (suf)
+ *  Returns true if str ends with suf
+ *)
 let ends_with str suf =
     let len_str = String.length str in
     let len_suf = String.length suf in
     len_str >= len_suf && String.sub str (len_str - len_suf) len_str = suf
 
+(* Returns the extension of a file name (everything after the last '.')
+ * Returns an empty string ("") if there is no extension
+ *)
 let extension fn =
-    let len_str = String.length fn in
-    let last_dot = String.rindex fn '.' in
-    String.sub fn (last_dot + 1) (len_str - last_dot - 1)
+    try
+        let len_str = String.length fn in
+        let last_dot = String.rindex fn '.' in
+        String.sub fn (last_dot + 1) (len_str - last_dot - 1)
+    with Not_found -> ""
